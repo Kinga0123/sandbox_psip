af70efde4e1f8a035d47e6646e703cacce31dc3b
Kinga0123 <149382131+Kinga0123@users.noreply.github.com>
2023-10-30 14:47:33 +0100

    add new functionalities

--- main.py
+++ main.py
@@ -2,7 +2,3 @@ from dane import users_list
 
 for user in users_list:
     print(f'Twój znajomy {user["name"]} dodał {user["posts"]}')
-
-
-
-# print(f'Twój znajomy {zmienna_na_dane[0]["nick"]} opublikował {zmienna_na_dane[0]["posts"]}!!!')
