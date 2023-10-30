54851d0adba642da4c11fc98232121ff874fa99d
Kinga0123 <149382131+Kinga0123@users.noreply.github.com>
2023-10-30 14:45:42 +0100

    add read functionalities

---
+++ dane.py
@@ -0,0 +1,11 @@
+users_list: list = [
+    {"name":"Kinga", "nick":"kinga","posts":100},
+    {"name":"Mateusz", "nick":"świetlik","posts":60},
+    {"name":"Bartosz", "nick":"Baran","posts":31231},
+    {"name":"Kamil", "nick":"Koc","posts":123},
+    {"name":"Marek", "nick":"Wisse","posts":0},
+    {"name":"Wiktoria", "nick":"Witka","posts":360},
+    {"name":"Wiktoria", "nick":"Wiki","posts":123},
+    {"name":"Monika", "nick":"Monia","posts":1_000_000},
+    {"name":"Kasia", "nick":"KKK","posts":1_500}
+]
---
+++ main.py
@@ -0,0 +1,8 @@
+from dane import users_list
+
+for user in users_list:
+    print(f'Twój znajomy {user["name"]} dodał {user["posts"]}')
+
+
+
+# print(f'Twój znajomy {zmienna_na_dane[0]["nick"]} opublikował {zmienna_na_dane[0]["posts"]}!!!')
